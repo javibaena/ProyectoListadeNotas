2c17e3dd6bef3dc00081b322f56ca540df0dc094
javibaena <59729766+javibaena@users.noreply.github.com>
2023-05-08 18:36:03 +0200

    Add files via upload

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,26 @@
-
 const mysql = require ("mysql");
  
  //conexion base de datos
 const conexion = mysql.createConnection({
 
-    host: "sql7.freesqldatabase.com",
+  /*   host: "sql7.freesqldatabase.com",
     port:  3306,
     user:"sql7615239",
     password:"MXWQR7ftSI",
-    database:"sql7615239"
-    /* host: "localhost",
+    database:"sql7615239" */
+
+
+  /*  host: "localhost",
     port: 8889,
     user:"root",
     password:"root",
     database:"crud" */
+
+    host: "sql8.freesqldatabase.com",
+    port:  3306,
+    user:"sql8616872",
+    password:"nFjLCpZhCu",
+    database:"sql8616872"
   });
   
   conexion.connect((err) => {
